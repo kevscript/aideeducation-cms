ae6fcb00d828ef83afadaae974ad037eb9136383
kevscript <kevscripted@gmail.com>
2023-10-13 23:11:11 +0200

    init payload

---
+++ src/server.ts
@@ -0,0 +1,30 @@
+import express from "express";
+import payload from "payload";
+
+require("dotenv").config();
+const app = express();
+const port = 3000;
+
+// Redirect root to Admin panel
+app.get("/", (_, res) => {
+  res.redirect("/admin");
+});
+
+const start = async () => {
+  // Initialize Payload
+  await payload.init({
+    secret: process.env.PAYLOAD_SECRET,
+    express: app,
+    onInit: async () => {
+      payload.logger.info(
+        `Payload listening at http://localhost:${port + payload.getAdminURL()}`
+      );
+    },
+  });
+
+  // Add your own express routes here
+
+  app.listen(port);
+};
+
+start();
